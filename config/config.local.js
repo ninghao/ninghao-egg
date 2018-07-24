bf4b0d0b490ef7981511d4c1b90263e628b45a67
wanghao8080 <117663444@qq.com>
2018-07-24 14:28:05 +0800

    数据库：MySQL / MariaDB

---
+++ config/config.local.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.mysql = {
+  client: {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'egg',
+  },
+};
--- config/plugin.js
+++ config/plugin.js
@@ -7,3 +7,8 @@ exports.nunjucks = {
   enable: true,
   package: 'egg-view-nunjucks',
 };
+
+exports.mysql = {
+  enable: true,
+  package: 'egg-mysql',
+};
